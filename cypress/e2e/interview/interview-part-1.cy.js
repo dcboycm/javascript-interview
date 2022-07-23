a5578ce48e86f97dd454e730daba1bbb325eda92
ccerda1402 <cerdajt@gmail.com>
2022-07-23 02:34:09 -0400

    express api

--- cypress/e2e/interview/interview-part-1.cy.js
+++ cypress/e2e/interview/interview-part-1.cy.js
@@ -1,12 +1,35 @@
 describe('Interview', () => {
     //  ----- Users API -----  \\
     it('When /users api is called with no params then it returns all users ordered by email ascending', () => {
-      // Todo
+      cy.request({
+        method: 'GET', 
+        url: 'http://localhost:3000/users', 
+        body: {
+        }
+      }).then( ({ body }) => {
+  
+        expect(body).to.have.length(50) // check number of users 
+        expect(body[0].email).to.eq('admitrs@altervista.org') // check first item's email value in response
+        expect(body[1].email).to.eq('akonzel12@ehow.com') // check second item's email value in response
+
+      })
+  
     })
     it('When /users api is called with id, email, or subscription_id param then it returns user', () => {
-      // Todo
-    })
+      cy.request({
+        method: 'GET', 
+        url: 'http://localhost:3000/users/id/5', 
+        body: {
+        }
+      }).then( ({ body }) => {
   
+        expect(body).to.have.length(1) // check that only one (id=5) item gets returned
+        expect(body[0].id).to.eq(5) // check that the id matches
+        expect(body[0].email).to.eq('cradnage4@salon.com') // check that the id matches
+
+
+      })
+    
   
     //  ----- Subscriptions API  -----  \\
     it('When /subscriptions api is called with id param then it returns subscription, user, and deliveries', () => {
@@ -24,4 +47,5 @@ describe('Interview', () => {
     it('When /deliveries api is called with subscription_id param then it returns all deliveries for given subscription_id', () => {
       // Todo
     })
+  })
 })
--- server.js
+++ server.js
@@ -9,11 +9,33 @@ const allSubscriptions = require('./data/subscriptions.json'); // import all sub
 const allDeliveries = require('./data/deliveries.json'); // import all deliveries data
 
 app.get('/users', (req, res) => {
-  res.status(200).json(allUsers)
+  let sortedUsers = allUsers
+  sortedUsers.sort((a, b) => a.email.localeCompare(b.email));
+  res.status(200).json(sortedUsers)
+})
+
+app.get('/users/id/:id', (req, res) => {
+  const user = allUsers.find(c => c.id === parseInt(req.params.id))
+  if(!user)res.status(404).send('User not found')
+  res.status(200).json([user])
+})
+
+app.get('/users/email/:email', (req, res) => {
+  const user = allUsers.find(c => c.email === (req.params.email))
+  if(!user)res.status(404).send('User not found')
+  res.status(200).json(user)
+})
+
+app.get('/users/subscription_id/:subscription_id', (req, res) => {
+  const user = allUsers.find(c => c.subscription_id === parseInt(req.params.subscription_id))
+  if(!user)res.status(404).send('User not found')
+  res.status(200).json(user)
 })
 
 app.get('/subscriptions', (req, res) => {
   res.status(200).json(allSubscriptions)
+
+
 })
 
 app.get('/deliveries', (req, res) => {
