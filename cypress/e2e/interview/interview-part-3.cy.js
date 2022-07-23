cc6d817f4b4744179071db9623e333e5d46f5960
ccerda1402 <cerdajt@gmail.com>
2022-07-23 01:09:11 -0400

    cypress test and test plan

--- cypress/e2e/interview/interview-part-3.cy.js
+++ cypress/e2e/interview/interview-part-3.cy.js
@@ -1,1 +1,42 @@
-// URL to start test with - 'https://www.nba.com/watch/nba-tv'
+
+const random = Math.random().toString(36).slice(2, 10);
+const email = random +"@gmail.com"; // randomize the email as the app validates for uniqueness
+
+describe('Interview', () => {
+    it('User can create account', () => {
+      cy.visit("https://www.nba.com/watch/nba-tv"); // navigate to nba-tv     
+    // there is likely a better way to handle this, like pre-filling in the session cookies, 
+    // but for purpose of test adding these lines to start test after the cookie pop up appears to avoid selector interruptions
+      cy.get('div#onetrust-policy', {timeout: 20000}).should('be.visible').then(() =>{
+          cy.get('button#onetrust-accept-btn-handler').should('be.visible').click();
+      });
+
+      cy.get('img[alt="nba tv"]', {timeout: 30000}).should('be.visible'); //wait for NBA banner to appear
+
+      // create user account
+      cy.get('div[data-text="SIGN IN"]').first().should('be.visible').click(); 
+      cy.get('.nba-login-link').contains('Sign Up').should('be.visible').click().then(() => {
+        cy.get('input#nbaSignUpModalId').click().type(email);
+        cy.get('input#nbaSignUpModalPw').click().type('secretPassword123');
+        cy.get('input#nbaSignUpModalId').click(); // click on email field so that password hint pop-up disappears
+        cy.get('input#nbaSignUpModalFN').click().type(random);
+        cy.get('input#nbaSignUpModalLN').click().type(random);
+        cy.get('select.nba-login-select').eq(0).select('2'); 
+        cy.get('select.nba-login-select').eq(1).select('2'); 
+        cy.get('select.nba-login-select').eq(2).select('1989'); 
+        cy.get('select.nba-login-select').eq(3).select('US'); 
+        cy.get('input#nbaSignUpModalPC').type('10031');
+        cy.get('label[for=nbaSignUpModalTAC]').eq(0).click();
+        cy.get('input#nbaSignUpModalCreate').click();
+      })
+
+      cy.get('.nba-login-body-favorite-teams-item', {timeout: 10000}).find('[data-text="GSW"]').click()
+      cy.get('.nba-login-next').contains('NEXT').click().then(() => {
+        cy.get('label[for=nbaNewsletters3]').eq(0).click();
+        cy.get('label[for=nbaNewsletters4]').eq(0).click();
+        cy.get('.nba-login-next').contains('NEXT').click();
+        cy.get('a[data-text="Not right now"]').should('be.visible').click();
+
+      })
+    })  
+})
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 
+const allUsers = require('./data/users.json'); // import all user data
+const allSubscriptions = require('./data/subscriptions.json'); // import all subscription data
+const allDeliveries = require('./data/deliveries.json'); // import all deliveries data
+
 app.get('/users', (req, res) => {
-  res.status(200).json({ text: 'Users!' })
+  res.status(200).json(allUsers)
 })
 
 app.get('/subscriptions', (req, res) => {
-  res.status(200).json({ text: 'Subscriptions!' })
+  res.status(200).json(allSubscriptions)
 })
 
 app.get('/deliveries', (req, res) => {
-  res.status(200).json({ text: 'Deliveries!' })
+  res.status(200).json(allDeliveries)
 })
 
 app.get('/', (req, res) => {
